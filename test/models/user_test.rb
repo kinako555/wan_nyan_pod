6f69ec1b9af15756e4bc8a8bf964d5e9c549bca9
kinako555 <dodohunter_3658@yahoo.co.jp>
2019-06-01 01:56:33 +0900

    20190601 commit

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = users(:one)
+  end
+
+  test "name should be present" do
+    @user.name = "     "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = "     "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
 end
