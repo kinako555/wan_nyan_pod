038baa2364d53823c1b80a883f844019ebba9106
kinako555 <dodohunter_3658@yahoo.co.jp>
2019-06-03 00:23:54 +0900

    complate create sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'users/new'
   # ヘルプページを作成する際はコメントを外す
-# get '/help',   to: 'static_pages#help'
-  get '/about',  to: 'static_pages#about'
-  get '/signup', to: 'users#new'
+# get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/signup',  to: 'users#new'
+  post '/signup', to: 'users#create'
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
