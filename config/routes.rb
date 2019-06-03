4833956ddc43a64aac6fa8eefabfa73f51cbdf77
kinako555 <dodohunter_3658@yahoo.co.jp>
2019-06-04 01:14:59 +0900

    20190604 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/new'
   # ヘルプページを作成する際はコメントを外す
-# get '/help',    to: 'static_pages#help'
-  get '/about',   to: 'static_pages#about'
-  get '/signup',  to: 'users#new'
-  post '/signup', to: 'users#create'
+# get    '/help',   to: 'static_pages#help'
+  get    '/about',  to: 'static_pages#about'
+  get    '/signup', to: 'users#new'
+  post   '/signup', to: 'users#create'
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
