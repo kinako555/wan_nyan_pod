f5b00699e1de0b75d355a08ed2686a96009cbef1
kinako555 <dodohunter_3658@yahoo.co.jp>
2019-06-12 23:49:56 +0900

    20190612 commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
     include SessionsHelper
+
+    private
+
+    # ログイン済か確認
+    # ログイン済みでなければ、ログイン画面へ遷移し、
+    # メッセージを表示
+    def logged_in_user
+        unless logged_in?
+          store_location
+          flash[:danger] = "ログインしてください"
+          redirect_to login_url
+        end
+    end
 end
