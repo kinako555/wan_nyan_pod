f0c0b72576095a2b097baf06df88e6e7aa4de057
kinako555 <dodohunter_3658@yahoo.co.jp>
2019-07-06 23:29:41 +0900

    seeds.rbにユーザー追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,14 @@ User.create!(name:  "Example User",
     activation_state: 'active',
     activation_token_expires_at: Time.zone.now)
 
+User.create!(name:  "わんにゃんぽっど太郎",
+    email: ENV['YAHOO_ADDRESS'],
+    password:              "foobar",
+    password_confirmation: "foobar",
+    admin: true,
+    activation_state: 'active',
+    activation_token_expires_at: Time.zone.now)
+
 30.times do |n|
     name  = Faker::Name.name
     email = "example-#{n+1}@railstutorial.org"
