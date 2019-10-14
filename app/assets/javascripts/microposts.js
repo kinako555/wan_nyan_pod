142744836524fe5e865bc7c1ad8aa14846a7c63a
kinako555 <dodohunter_3658@yahoo.co.jp>
2019-10-15 08:22:25 +0900

    謎は深まっていく

--- app/assets/javascripts/microposts.js
+++ app/assets/javascripts/microposts.js
@@ -1,21 +1,30 @@
 $(function() {
 
+    let selected_pictures_length = 0;
+
     // 新規作成
 
     $('.open_new_micropost').on('click', function() {
         $('#modal_new_micropost').modal(); // modal表示
     });
 
-    $('#micropost_pictures').on('change',function(e){
+    $('#micropost_pictures').on('change',function(e) {
         const ERROR_MSG = '5MB以下のファイルを選択してください。';
         let files = e.target.files;
         let def = (new $.Deferred()).resolve();
         // 画像ファイルサイズチェック
         if (!isCheckSizeMb(files)) {
             alert(ERROR_MSG);
-            return;
+            return false;
         }
-        $.each(files,function(i,file){
+        
+        if (!isCheckPicturesLength(selected_pictures_length + files.length)) {
+            alert('画像は4枚まで選択できます。');
+            return false;
+        }
+        selected_pictures_length += files.length;
+        formatModalSize(selected_pictures_length);
+        $.each(files,function(i,file){      
             def = def.then(function(){return previewPicture(file)});
         });
     })
@@ -27,8 +36,18 @@ $(function() {
         });
         return true;
     }
-    
-    let previewPicture = function(imageFile){
+
+    function isCheckPicturesLength(length) {
+        return (length <= 4 ? true : false);
+    }
+
+    // 選択画像枚数に合わせてモーダルサイズを調整
+    function formatModalSize(length) {
+        const DEFAULT_HEIGHT = length * 250 + 498;
+        $('.modal-content').css('height', DEFAULT_HEIGHT + 'px')
+    }
+
+    function previewPicture(imageFile) {
         let reader = new FileReader();
         let img = new Image();
         let def =$.Deferred();
@@ -40,6 +59,7 @@ $(function() {
         reader.readAsDataURL(imageFile);
         return def.promise();
     }
+
     // 一覧
 
     const FAVORITE_SUBMIT_ID_FIRST = '#favorite_micropost_submit-';
